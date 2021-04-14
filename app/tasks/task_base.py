05f1447c4854da094c364e19069b097f1b892a35
tongchengbin <58296672@qq.com>
2021-04-14 14:39:37 +0800

    fix bug: 定时任务异常

--- app/tasks/task_base.py
+++ app/tasks/task_base.py
@@ -13,13 +13,12 @@ def day_upload_req():
 
         定时任务 每天执行一次    将redis内的数据写入db
     """
-    with flask_app.app_context():
-        day = (datetime.today() - timedelta(days=1)).strftime("%Y%m%d")
-        ip_count = cache.scard(ConstCacheKey.IP_DAY_SET % day)
-        req_count = int(cache.get(ConstCacheKey.REQ_DAY_COUNT % day) or 0)
-        instance = RequestState(day=day, ip_count=ip_count, req_count=req_count)
-        db.session.add(instance)
-        db.session.commit()
+    day = (datetime.today() - timedelta(days=1)).strftime("%Y%m%d")
+    ip_count = cache.scard(ConstCacheKey.IP_DAY_SET % day)
+    req_count = int(cache.get(ConstCacheKey.REQ_DAY_COUNT % day) or 0)
+    instance = RequestState(day=day, ip_count=ip_count, req_count=req_count)
+    db.session.add(instance)
+    db.session.commit()
 
 
 @celery_app.task
