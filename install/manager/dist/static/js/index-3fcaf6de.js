3327f98d6aff9ce3429ebd8cf9921cf1927052f5
tongc <58296672@qq.com>
2023-09-09 01:36:04 +0800

    build dist

--- install/manager/dist/static/js/index-3fcaf6de.js
+++
@@ -1,1 +0,0 @@
-import{e,o as n,c as o}from"./index-cdc3a115.js";const r=e({name:"Welcome",__name:"index",setup(a){return(c,t)=>(n(),o("h1",null,"Ocean Admin"))}});export{r as default};
---
+++ install/manager/dist/static/js/redirect-e2b95ecc.js
@@ -0,0 +1,1 @@
+import{e as c,x as s,l as p,c as u,o as _}from"./index-5ba26d12.js";const f=c({name:"Redirect",__name:"redirect",setup(i){const{currentRoute:r,replace:t}=s(),{params:a,query:n}=p(r),{path:e}=a,o=Array.isArray(e)?e.join("/"):e;return t({path:"/"+o,query:n}),(m,l)=>(_(),u("div"))}});export{f as default};
