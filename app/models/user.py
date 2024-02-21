f16c026341636303957c444fd085138b8b628a25
tongchengbin <58296672@qq.com>
2024-02-21 14:08:59 +0800

    fix datalong

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,6 @@ from app.extensions import db
 class User(MainBase):
     __tablename__ = 'user'
     username = Column(db.String(256), unique=True, nullable=False, comment='用户名')
-    password = Column(db.String(128), nullable=False, comment='密码')
+    password = Column(db.String(512), nullable=False, comment='密码')
     active = Column(db.Boolean(),default=True, comment="是否启用")
     token = Column(db.String(64), comment="token", nullable=True)
