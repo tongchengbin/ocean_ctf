e2884ad12cf1b589f62dc6aa0fec1db2a775dcf2
tongchengbin <58296672@qq.com>
2024-03-04 09:38:51 +0800

    修复表单验证错误 fixed #47

--- app/ctf/form.py
+++ app/ctf/form.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class QuestionForm(BaseModel):
     name: str
     active: bool
-    active_flag: bool
+    active_flag: Optional[bool]
     flag: Optional[str]
     desc: str
     type: str
